0247f69c36b725d5896148b13360a10d2540de4f
a3magic3pocket <a3magic3pocket@gmail.com>
2023-03-23 18:11:03 +0900

    tmp commit

--- client.py
+++ client.py
@@ -4,16 +4,23 @@ import asyncio
 import websockets
 import gzip
 import numpy as np
+from PIL import Image
+import io
 import ssl
 
 async def hello():
-    # uri = "ws://localhost:8765"
+    uri = "ws://localhost:8765"
     # async with websockets.connect(uri) as websocket:
     # uri = "wss://0l75dgay7yn-496ff2e9c6d22116-8765-colab.googleusercontent.com"
-    uri = "wss://0l75dgay7yn-496ff2e9c6d22116-8765-colab.googleusercontent.com/"
+    # uri = "wss://0l75dgay7yn-496ff2e9c6d22116-8765-colab.googleusercontent.com/"
     async with websockets.connect(uri, origin="*") as websocket:
-        data = np.array([1,2,3,4], dtype=np.float32)
-        print(f"{data=}")
+        img = Image.open('test.png').convert("L")
+        data = np.array(img, dtype=np.uint8)
+        print('data', data)
+        print('data', data.shape)
+        
+        # data = np.array([1,2,3,4], dtype=np.float32)
+        # print(f"{data=}")
         compressed = gzip.compress(data)
         
         # name = input("What's your name? ")
--- server/server.py
+++ server/server.py
@@ -1,23 +1,62 @@
 #!/usr/bin/env python
 
+from PIL import Image
+from crnn import detect as crnn_detect
 import asyncio
 import websockets
 import gzip
 import numpy as np
+import sys 
+
 
 async def hello(websocket):
     compressed = await websocket.recv()
     data = gzip.decompress(compressed)
-    converted = np.frombuffer(data, dtype=np.float32)
 
-    greeting = f"Hello! got {converted}"
+    converted = np.frombuffer(data, dtype=np.uint32)
+    print(f'{converted.shape=}')
+    velocity_img = Image.fromarray(converted)
+    # velocity_img = await get_current_velocity(converted)
+    print(velocity_img)
+    # velocity_img.convert('L').save('testtest.jpeg')
+    import sys
+    sys.exit(1)
+
+    greeting = f"Hello! got {velocity_img}"
+    print(crnn_detect.detect(velocity_img, './crnn/checkpoints/exp1/best.ckpt'))
 
     await websocket.send(greeting)
     print(f">>> {greeting}")
 
+
 async def main():
-    async with websockets.serve(hello, "localhost", 8765):
+    # about 100MB
+    max_size = 100 * 2**20
+    assert sys.maxsize > max_size
+    print(f'{sys.maxsize=}')
+    print(f'{max_size=}')
+    
+    async with websockets.serve(
+        hello,
+        "localhost",
+        8765,
+        max_size=max_size,
+    ):
         await asyncio.Future()  # run forever
 
+async def get_current_velocity(img):
+    w, h = img.size
+    left_rate = 81 / 1280
+    upper_rate = 220 / 720
+    right_rate = 125 / 1280
+    lower_rate = 236 / 720
+    
+    return img.crop((
+        left_rate * w,
+        upper_rate * h,
+        right_rate * w,
+        lower_rate * h,
+    ))
+    
 if __name__ == "__main__":
     asyncio.run(main())
